3e6b1bdbf19b63f75ffe6589aee32bd95dd7f744
Alisa Filatova <alicefill@bk.ru>
2019-06-13 13:14:36 +0300

    Личный проект: ещё больше подробностей

--- src/components/offers-list/offers-list.jsx
+++ src/components/offers-list/offers-list.jsx
@@ -20,41 +20,9 @@ const OffersList = ({rentalOffers, setActiveItem, onOfferTitleClick}) => (
 );
 
 OffersList.propTypes = {
-  rentalOffers: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number,
-    title: PropTypes.string,
-    [`preview_image`]: PropTypes.string,
-    images: PropTypes.arrayOf(PropTypes.string),
-    [`is_premium`]: PropTypes.bool,
-    [`is_favorite`]: PropTypes.bool,
-    bedrooms: PropTypes.number,
-    goods: PropTypes.arrayOf(PropTypes.string),
-    description: PropTypes.string,
-    price: PropTypes.number,
-    rating: PropTypes.number,
-    type: PropTypes.string,
-    location: PropTypes.shape({
-      latitude: PropTypes.number,
-      longitude: PropTypes.number,
-      zoom: PropTypes.number,
-    }),
-    city: PropTypes.shape({
-      name: PropTypes.string,
-      location: PropTypes.shape({
-        latitude: PropTypes.number,
-        longitude: PropTypes.number,
-        zoom: PropTypes.number,
-      }),
-    }),
-    host: PropTypes.shape({
-      id: PropTypes.number,
-      name: PropTypes.string,
-      [`is_pro`]: PropTypes.bool,
-      [`avatar_url`]: PropTypes.string,
-    }),
-  })).isRequired,
-  setActiveItem: PropTypes.func.isRequired,
-  onOfferTitleClick: PropTypes.func.isRequired,
+  rentalOffers: PropTypes.arrayOf(PropTypes.object).isRequired,
+  setActiveItem: PropTypes.func,
+  onOfferTitleClick: PropTypes.func,
 };
 
 export default OffersList;
